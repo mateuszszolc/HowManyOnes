2e959f48c1deb3d9de0ca7f39c8f431a1cba8500
mateuszszolc <51001128+mateuszszolc@users.noreply.github.com>
2020-05-27 00:26:55 +0200

    Initial commit

---
+++ HowOnes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowOnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę: ");
+            int n = int.Parse(Console.ReadLine());
+            List<string> strList = Enumerable.Range(1, n).ToList().ConvertAll(Convert.ToString);
+            int outcome = 0;
+            foreach(var item in strList)
+            {
+                outcome += item.Count(j => j == '1');
+            }
+            Console.WriteLine(outcome);
+            Console.ReadKey();
+        }
+    }
+}
